e6c882f9e4638d45c878c121b190b2ce849e18ca
Víctor Muñoz Sánchez <victormu98@Victors-MacBook-Pro.local>
2022-01-26 17:04:12 +0100

    implemented not working program

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
+from re import A
 from core.logger import Logger
 from src import network as net
 from src import data
-from src.loss_functions import CategoricalCrossEntropyLoss
 
 
 def main():
 
     logger = Logger(name='main')
 
-    x, y = data.vertical(samples=100, classes=3)
+    samples, targets = data.vertical(samples=100, classes=3)
 
-    inputSize = x.shape[1]
-    input = x
+    inputSize = samples.shape[1]
 
     layer1 = net.Layer(inputSize=inputSize, neurons=3)
     layer2 = net.Layer(inputSize=layer1.neurons, neurons=3)
 
-    layer1.forward(input)
+    layer1.forward(samples)
     layer2.forward(
         layer1.output, activationFunction=net.ActivationFunctions.softmax)
 
-    loss = CategoricalCrossEntropyLoss.calculate(layer2.output, y)
+    loss = net.CategoricalCrossEntropyLoss.calculate(layer2.output, targets)
 
-    logger.debug('output: ', layer2.output[:5])
-    logger.debug('loss: ', loss)
+    logger.debug('output:\n', layer2.output[:5])
+    logger.debug('loss:\n', loss)
+
+    layer2.backward(layer2.output, targets,
+                    activationFunction=net.ActivationFunctions.softmax)
+    layer1.backward(layer2.error)
+
+    logger.debug('layer1 dweights:\n', layer1.dweights)
+    logger.debug('layer1 dbiases:\n', layer1.dbiases)
+    logger.debug('layer2 dweights:\n', layer2.dweights)
+    logger.debug('layer2 dbiases:\n', layer2.dbiases)
 
 
 if __name__ == '__main__':
--- src/network.py
+++ src/network.py
@@ -1,3 +1,4 @@
+from os import stat
 import numpy as np
 import enum
 
@@ -7,16 +8,54 @@ class ActivationFunctions(enum.Enum):
     softmax = enum.auto()
 
 
-class ActivationFunctionsImplementation:
+class _ActivationRelu:
     @staticmethod
-    def relu(inputs):
+    def forward(inputs):
         return np.maximum(0, inputs)
 
     @staticmethod
-    def softmax(inputs):
+    def backward(inputs):
+        output = inputs.copy()
+        output[inputs <= 0] = 0
+
+        return output
+
+
+class _ActivationSoftmax:
+    @staticmethod
+    def forward(inputs):
         exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         return probabilities
+
+    @staticmethod
+    def backward(predictions, targets):
+
+        if len(targets.shape) == 2:
+            targets = np.argmax(targets, axis=1)
+
+        output = predictions.copy()
+
+        output[range(len(predictions)), targets] -= 1
+        output = output / len(predictions)
+
+        return output
+
+
+class CategoricalCrossEntropyLoss:
+    @staticmethod
+    def calculate(predictions, targets):
+        predictions = np.clip(predictions, 1e-7, 1-1e-7)
+
+        if len(targets.shape) == 1:
+            correctConfidences = predictions[range(len(predictions)), targets]
+        elif len(targets.shape) == 2:
+            correctConfidences = np.sum(predictions*targets, axis=1)
+
+        likelihoods = -np.log(correctConfidences)
+        loss = np.mean(likelihoods)
+
+        return loss
 
 
 class Layer:
@@ -25,14 +64,30 @@ class Layer:
         self.weights = 0.1 * np.random.randn(inputSize, neurons)
         self.biases = np.zeros((1, neurons))
 
-    def forward(self, inputs: int, activationFunction=ActivationFunctions.relu):
+    def forward(self, inputs, activationFunction=ActivationFunctions.relu):
+        self.inputs = inputs
         output = np.dot(inputs, self.weights) + self.biases
-        self.output = self._pickActivationFunction(output, activationFunction)
+        self.output = self._pickForwardFunction(activationFunction, output)
+
+    def backward(self, error, targets=None, activationFunction=ActivationFunctions.relu):
+        activationBackward = self._pickBackwardFunction(
+            activationFunction, error, targets)
+        self.dbiases = np.sum(activationBackward, axis=0, keepdims=True)
+        self.dweights = np.dot(self.inputs.T, error)
+        self.error = np.dot(error, self.weights.T)
 
     @staticmethod
-    def _pickActivationFunction(inputs, activationFunction):
+    def _pickForwardFunction(activationFunction, inputs):
         if activationFunction is ActivationFunctions.relu:
-            return ActivationFunctionsImplementation.relu(inputs)
+            return _ActivationRelu.forward(inputs)
 
         elif activationFunction is ActivationFunctions.softmax:
-            return ActivationFunctionsImplementation.softmax(inputs)
+            return _ActivationSoftmax.forward(inputs)
+
+    @staticmethod
+    def _pickBackwardFunction(activationFunction, error, targets):
+        if activationFunction is ActivationFunctions.relu:
+            return _ActivationRelu.backward(error)
+
+        elif activationFunction is ActivationFunctions.softmax:
+            return _ActivationSoftmax.backward(error, targets)
